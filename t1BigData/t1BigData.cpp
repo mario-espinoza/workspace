49b622d54369cb4898436b36812eff0235b4b981
mespinozas <mario.espinoza@gmail.com>
2015-04-22 01:20:34 -0300

    ultimo con logs

--- t1BigData/t1BigData.cpp
+++ t1BigData/t1BigData.cpp
@@ -84,7 +84,7 @@ double getCPUUsage(){
 		i=fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
 		fclose(fp);
 		loadavg = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
-		return loadavg*100;
+		return loadavg;
 	}
 }
 
